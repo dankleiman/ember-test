f6962e21d7fe8094ec86f6db371724811f2e7f30
Dan Kleiman <info@dankleiman.com>
2014-07-15 08:35:38 -0400

    Editing leads

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,4 +1,5 @@
 App.LeadController = Ember.ObjectController.extend({
+  isEditing: false,
   showUnsavedMessage: function(){
     return this.get('isDirty') && !this.get('isSaving');
   }.property('isDirty', 'isSaving'),
